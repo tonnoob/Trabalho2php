12f9984caf661a0ecd99bfe254281709d684b842
tonnoob <ottonsouzanto@gmail.com>
2022-11-25 19:43:49 -0300

    Adcionando o trabalho no repositório

---
+++ cadastro.php
@@ -0,0 +1,72 @@
+<section>
+    <form>
+        <div>
+            <input type="text" placeholder="Nome: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="Nome do pai: " />
+        </div>    
+
+        <div>
+            <input type="text" placeholder="Nome da mãe: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="Sexo: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="CPF: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="RG: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="Cidade de Nasc. - UF: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="País de Nascimento: " />
+        </div>
+        
+        <div>
+            <input type="text" placeholder="Data de Nascimento: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="RA do Aluno:: " />
+        </div>
+        
+    </form>
+</section>
+
+<section>
+    <form>
+        <div>
+            <input type="text" placeholder="Endereço - Nº: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="Complemento: " />
+        </div>    
+
+        <div>
+            <input type="text" placeholder="Bairro: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="CEP: " />
+        </div>
+
+        <div>
+            <input type="text" placeholder="Cidade - UF: " />
+        
+        </div>
+        <div>
+            <input type="submit" value="Cadastrar" />
+        </div>
+    </form>
+</section>
